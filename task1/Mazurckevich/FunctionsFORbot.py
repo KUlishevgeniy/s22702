3cd58c2597360d682445fbf50be7f101e79eca53
Mazurckevich <maz_2005@list.ru>
2023-05-22 19:39:18 +0300

    yes

--- task1/Mazurckevich/FunctionsFORbot.py
+++
@@ -1,5 +0,0 @@
-def add(x,y):
-    a=123
-    sd='sdsds'
-    return x+y+a
-print(add(1,2))
--- task1/Mazurckevich/functions.py
+++
@@ -1,5 +0,0 @@
-def summ(a,b):
-    c=a+b
-    return c
-#в другом файле пишем:
-#from functions import summ
